191b636b580f40be4281c09027af208c4b7a256c
Bmijin <bmj13465@jtts.co.kr>
2024-10-24 10:12:33 +0900

    서블릿 기본기

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan		// 서블릿 자동 등록 : 하위 패키지에서 서블릿을 다 찾아서 자동으로 서블릿을 저장해주고 실행해준다.
 @SpringBootApplication
 public class ServletApplication {
 
