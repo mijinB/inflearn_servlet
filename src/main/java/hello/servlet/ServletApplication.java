4545938217c13cad19ddb6d20aa2f97d6a56e37f
Bmijin <bmj13465@jtts.co.kr>
2024-11-08 10:59:43 +0900

    뷰 리졸버

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan		// 서블릿 자동 등록 : 하위 패키지에서 서블릿을 다 찾아서 자동으로 서블릿을 저장해주고 실행해준다.
 @SpringBootApplication
@@ -11,5 +14,12 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+	/*
+	Spring Boot 가 자동으로 해주는 ViewResolver
+	@Bean
+	ViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+	}*/
 
 }
--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -16,6 +16,13 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+
+        /*
+        뷰 리졸버 - InternalResourceViewResolver
+        : 스프링 부트는 InternalResourceViewResolver 라는 뷰 리졸버를 자동으로 등록하는데,
+          이때 application.properties 에 등록한 spring.mvc.view.prefix , spring.mvc.view.suffix 설정 정보를 사용해서 등록한다.
+           ⇒ ServletApplication 파일의 주석 ViewResolver 참고
+         */
+        return new ModelAndView("new-form");
     }
 }
